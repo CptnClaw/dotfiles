feef15cd883200210dc118e4125fb94813e9f156
Eyal Seelig <eyal.seelig@gmail.com>
2024-07-24 16:41:16 -0700

    Added keyboard shortcuts to vscode-neovim

--- .config/nvim/init.vscode.lua
+++ .config/nvim/init.vscode.lua
@@ -4,4 +4,8 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Key mappings
-vim.api.nvim_set_keymap('n', '<leader>h', ':set hlsearch!<CR>', {})
+vim.keymap.set('n', '<leader>h', ':set hlsearch!<CR>')
+vim.keymap.set({'n', 'v'}, '<leader>f', function() require('vscode').call('editor.action.formatSelection') end)
+vim.keymap.set({'n', 'v'}, '<leader>p', function() require('vscode').call('workbench.action.showCommands') end)
+vim.keymap.set({'n', 'v'}, '<leader>o', function() require('vscode').call('workbench.action.quickOpen') end)
+
